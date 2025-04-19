4a729a08938c99eaa6a28a2e18b351289bce3e15
gymm11 <pubgindia.aryan@gmail.com>
2025-04-19 13:28:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '7972439998:AAH35ZuNPI-p4MWR5i_9883aRQA7sH19WDM'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
-AUTHORIZED_USERS = [5588464519]
+AUTHORIZED_USERS = [1374254770]
 
 # 🌐 Global dictionary to keep track of user attacks
 user_attacks = {}
